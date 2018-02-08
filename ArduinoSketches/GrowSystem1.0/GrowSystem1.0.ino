f43250481e49d992972009beeddf13e1528eebe7
Fynn <Fynn@DESKTOP-7JDICME.fritz.box>
2018-02-08 21:35:46 +0100

    added timestamp

--- ArduinoSketches/GrowSystem1.0/GrowSystem1.0.ino
+++ ArduinoSketches/GrowSystem1.0/GrowSystem1.0.ino
@@ -1,5 +1,12 @@
+
+#include <TimeLib.h>
+
+
 #include <dht.h> //Importing lib for DHT11
 #include <LiquidCrystal.h> //Importing lib for LCDScreen
+
+#define TIME_HEADER "T"
+#define TIME_REQUEST 7
 
 //LCD
 const int rs = 13, en = 12, d4 = 7, d5 = 6, d6 = 5, d7 = 4; //Pins for LCD on arduino
@@ -23,15 +30,21 @@ int l1_value = 0; //Lightphotoresistor value
 void setup() {
   Serial.begin(9600); //Initialize the serial monitor
   lcd.begin(16, 2); //Initialize Colums(16), Initialize Rows(2)
-  Serial.println("GrowSystem works. Getting data... vers: 1.0");
+  setSyncProvider(requestSync); //set function to call when sync required
+  Serial.println("GrowSystem vers:1.0 Getting data...");
   Serial.println("-------------------------------------------");
 }
 
 void loop() {
   //if (Serial.available()) {
+    processSyncMessage();
+    if (timeStatus() != timeNotSet) {
+      digitalClockDisplay();
+    }
     getM1Value();
     getDhtValue();
     getLightValue();
+    digitalClockDisplay();
     Serial.println("-------------------------------------------");
     Serial.println("Data got. Finishes the loop");
     Serial.println("-------------------------------------------");
@@ -94,8 +107,41 @@ void LightonLCD() {
   lcd.setCursor(6, 0); //Sets the cursor on colum "0", row "1"
   lcd.print(l1_value);
 }
+void digitalClockDisplay(){
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.print(" ");
+  Serial.print(day());
+  Serial.print(" ");
+  Serial.print(month());
+  Serial.print(" ");
+  Serial.print(year()); 
+  Serial.println(); 
+}
+void printDigits(int digits){
+  // utility function for digital clock display: prints preceding colon and leading 0
+  Serial.print(":");
+  if(digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
+}
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
 
-
-
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
+  }
+}
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
+}
 
 
