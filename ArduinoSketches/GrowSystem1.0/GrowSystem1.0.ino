a9765b7bc7a737fc8347be039eda24955bcbd04a
Fynn <Fynn@DESKTOP-7JDICME.fritz.box>
2018-02-11 04:29:05 +0100

    edit

--- ArduinoSketches/GrowSystem1.0/GrowSystem1.0.ino
+++ ArduinoSketches/GrowSystem1.0/GrowSystem1.0.ino
@@ -1,10 +1,13 @@
+#include <DS3231.h>
 #include <dht.h> //Importing lib for DHT11
 #include <LiquidCrystal.h> //Importing lib for LCDScreen
-
 
 //LCD
 const int rs = 13, en = 12, d4 = 7, d5 = 6, d6 = 5, d7 = 4; //Pins for LCD on arduino
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7); //Object lcd initialize
+
+//Real-Time-Clock
+DS3231  rtc(SDA, SCL);
 
 //Moisture Sensor
 int m1_control = 52; //Pin for the transistor on arduino
@@ -24,7 +27,9 @@ int l1_value = 0; //Lightphotoresistor value
 void setup() {
   Serial.begin(9600); //Initialize the serial monitor
   lcd.begin(16, 2); //Initialize Colums(16), Initialize Rows(2)
- 
+  rtc.begin(); //Initalize Real-Time-Clock object
+  rtc.setDate(11, 02, 2018);
+  rtc.setTime(4, 5, 0);
   Serial.println("GrowSystem vers:1.0 Getting data...");
   Serial.println("-------------------------------------------");
 }
@@ -33,6 +38,7 @@ void loop() {
   getM1Value();
   getDhtValue();
   getLightValue();
+  getTimeStamp();
   Serial.println("-------------------------------------------");
   Serial.println("Data got. Finishes the loop");
   Serial.println("-------------------------------------------");
@@ -50,50 +56,64 @@ void getM1Value() {
   }
   digitalWrite(m1_control, LOW); //Sensor off!
   m1_value = avg / 100;
-  Serial.print("M1:");
+  Serial.print("m1:");
   Serial.println(m1_value);
   M1onLCD();
   delay(3000);
 }
 void getDhtValue() {
   int chk = DHT.read11(DHT11_PIN);
-  t_value = DHT.temperature;
-  h_value = DHT.humidity;
-  Serial.print("Temperatur:");
-  Serial.println(t_value, DEC);
-  Serial.print("L-Feuchtigkeit:");
-  Serial.println(h_value, DEC);
+  t_value = DHT.temperature/100;
+  h_value = DHT.humidity/100;
+  Serial.print("t1:");
+  Serial.println(t_value);
+  Serial.print("h1:");
+  Serial.println(h_value);
   DhtonLCD();
   delay(3000);
 }
 void getLightValue() {
   l1_value = analogRead(l1_sensor);
-  Serial.print("Licht:");
+  Serial.print("l1:");
   Serial.println(l1_value);
   LightonLCD();
   delay(3000);
 }
 void M1onLCD() {
   lcd.clear();
-  lcd.print("F-Wert:");
+  lcd.print("M1:");
   lcd.setCursor(0, 1); //Sets Cursor on colum "0", row "1"
   lcd.print(m1_value);
 }
 void DhtonLCD() {
   lcd.clear();
-  lcd.print("T:");
+  lcd.print("T1:");
   lcd.setCursor(3, 0); //Sets the cursor on colum "0" , row "1"
   lcd.print(t_value);
   lcd.setCursor(0, 1);
-  lcd.print("H:");
+  lcd.print("H1:");
   lcd.setCursor(3, 1);
   lcd.print(h_value);
 }
 void LightonLCD() {
   lcd.clear();
-  lcd.print("Light:");
-  lcd.setCursor(6, 0); //Sets the cursor on colum "0", row "1"
+  lcd.print("L1:");
+  lcd.setCursor(7, 0); //Sets the cursor on colum "0", row "1"
   lcd.print(l1_value);
+}
+void getTimeStamp() {
+  lcd.setCursor(0, 0);
+  lcd.print("Time:  ");
+  lcd.print(rtc.getTimeStr());
+  lcd.setCursor(0, 1);
+  lcd.print("Date: ");
+  lcd.print(rtc.getDateStr());
+  Serial.print("Time:  ");
+  Serial.println(rtc.getTimeStr());
+  Serial.print("Date: ");
+  Serial.println(rtc.getDateStr());
+  delay(3000);
+
 }
 
 
--- src/Arduino.py
+++ src/Arduino.py
@@ -6,26 +6,26 @@ Created on 04.02.2018
 import serial
 import time
 from Time import Time
-class getData():
-    for com in range(0, 4):
+      
+for com in range(0, 4):
+    try:
+        PORT = '/dev/ttyACM' + str(com)
+        BAUD = 9600
+        board = serial.Serial(PORT, BAUD)
+        print('Arduino erkannt an /dev/ttyACM' + str(com))
+        print('Hardware: %s' % board.__str__())
+        board.isOpen()
+        time.sleep(5)
+        board.write("test")
+        response = ''
         try:
-            PORT = '/dev/ttyACM' + str(com)
-            BAUD = 9600
-            board = serial.Serial(PORT, BAUD)
-            print('Arduino erkannt an /dev/ttyACM' + str(com))
-            print('Hardware: %s' % board.__str__())
-            board.isOpen()
-            time.sleep(5)
-            board.write("test")
-            response = ''
-            try:
-                while True:  
-                    response += board.readline() + '\n'
-                    if "Light:" in response:
-                        print(response)
-                        break
-            except KeyboardInterrupt:     
-                        board.close()
-        except:
+            while True:  
+                response += board.readline() + '\n'
+                if "m1:" and "Date:" in response:
+                    print(response)
+                    break
+        except KeyboardInterrupt:     
             board.close()
+    except:
+        board.close()
 
