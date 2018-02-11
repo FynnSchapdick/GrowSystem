5fba7635c9335b21deb32cf7caa26af38791ede7
Fynn <Fynn@DESKTOP-7JDICME.fritz.box>
2018-02-11 01:08:23 +0100

    edit

--- src/Arduino.py
+++ src/Arduino.py
@@ -6,29 +6,25 @@ Created on 04.02.2018
 import serial
 import time
 from Time import Time
-
-def getData():
-    for com in range(0,4):
+for com in range(0, 4):
+    try:
+        PORT = '/dev/ttyACM' + str(com)
+        BAUD = 9600
+        board = serial.Serial(PORT, BAUD)
+        print('Arduino erkannt an /dev/ttyACM' + str(com))
+        print('Hardware: %s' % board.__str__())
+        board.isOpen()
+        time.sleep(5)
+        board.write("test")
+        response = ''
         try:
-            PORT = '/dev/ttyACM' + str(com)
-            BAUD = 9600
-            board = serial.Serial(PORT,BAUD)
-            print('Arduino erkannt an /dev/ttyACM' + str(com))
-            print('Hardware: %s' % board.__str__())
-            board.isOpen()
-            time.sleep(5)
-            board.write("test")
-            response=''
-            try:
-                while True:  
-                    response += board.readline() + '\n'
-                    if "M1" in response:
-                        print(response)
-                        break
-            except KeyboardInterrupt:     
+            while True:  
+                response += board.readline() + '\n'
+                if "Light:" in response:
+                    print(response)
+                    break
+        except KeyboardInterrupt:     
                 board.close()
-        except:
+    except:
             board.close()
-
-
 
