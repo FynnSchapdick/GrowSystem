a3c0da47497d53dc64b39178373dcbde44474eaf
Fynn <Fynn@DESKTOP-7JDICME.fritz.box>
2018-02-14 09:51:25 +0100

    editet

--- src/Arduino.py
+++ src/Arduino.py
@@ -5,40 +5,30 @@ Created on 04.02.2018
 '''
 import serial
 import time
-import xml.etree.cElementTree as ET
-from Time import Time
-      
-for com in range(0, 4):
-    try:
-        PORT = '/dev/ttyACM' + str(com)
-        BAUD = 9600
-        board = serial.Serial(PORT, BAUD)
-        print('Arduino erkannt an /dev/ttyACM' + str(com))
-        print('Hardware: %s' % board.__str__())
-        board.isOpen()
-        time.sleep(5)
-        board.write("test")
-        response = ''
-        try:
-            while True:  
-                response += board.readline() + '\n'
-                if "Start" and "End" in response:
-                    print(response)
-                    break
-        except KeyboardInterrupt:     
-            board.close()
-    except:
-        board.close()
 
-words = response.split('Start')
-word = words[1]
-words = word.split('End')
-word = words[0]
-words = word.split(' ')
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-for i in range(0,len(words),2):
-    ET.SubElement(doc,"data",name=words[i]).text = words[i+1]
-tree = ET.ElementTree(root)
-tree.write("data.xml")
+class Arduino:
+    
+    def readData(self):
+              
+        for com in range(0, 4):
+            try:
+                PORT = '/dev/ttyACM' + str(com)
+                BAUD = 9600
+                board = serial.Serial(PORT, BAUD)
+                print('Arduino erkannt an /dev/ttyACM' + str(com))
+                print('Hardware: %s' % board.__str__())
+                board.isOpen()
+                time.sleep(5)
+                board.write("test")
+                response = ''
+                try:
+                    while True:  
+                        response += board.readline() + '\n'
+                        if "Start" and "End" in response:
+                            print(response)
+                            break
+                except KeyboardInterrupt:     
+                    board.close()
+            except:
+                board.close()
 
--- src/GUI.py
+++ src/GUI.py
@@ -3,30 +3,37 @@ Created on 11.02.2018
 
 @author: Fynn
 '''
-import Arduino as getData
+
 import tkinter as tk
+from tkinter import ttk
+from tkinter import Tk
 import Arduino
+import Data
 
 LARGE_FONT = ("Verdana", 12)
 
 class GrowSystemapp(tk.Tk):
     
     def __init__(self,*args,**kwargs):
+        
         tk.Tk.__init__(self,*args,**kwargs)
+        
+        #tk.Tk.iconbitmap(self, default="C:\Users\Fynn\Downloads\2014-11-13_15-23-06-546.png")
+        tk.Tk.wm_title(self, 'GrowSystem 0.0.1')
         container = tk.Frame(self)
-        
         container.pack(side="top",fill="both",expand=True)
-        
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
         
         self.frames = {}
         
-        frame = StartPage(container, self)
-        
-        self.frames[StartPage] = frame
-        
-        frame.grid(row=0,column=0,sticky="nsew")
+        for F in (StartPage, PageMeasurements):
+                
+            frame = F(container, self)
+            
+            self.frames[F] = frame
+            
+            frame.grid(row=0,column=0,sticky="nsew")
         
         self.show_frame(StartPage)
         
@@ -37,20 +44,33 @@ class GrowSystemapp(tk.Tk):
         frame.tkraise()
                 
                 
-def qf(self):
-    Arduino.getData
            
 class StartPage(tk.Frame):
     
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self,text="Grow System", font=LARGE_FONT)
-        
         label.pack(pady=10,padx=10)
-        
-        button1 = tk.Button(self,text="Getting Values",command=qf)
+        button1 = tk.Button(self,text="Measurements",command=lambda: controller.show_frame(PageMeasurements))
         button1.pack()
         
+class PageMeasurements(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self,text="Measurements", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        
+        #containerM = tk.Frame(self)
+        
+        button1 = tk.Button(self,text="Back to Home",command=lambda: controller.show_frame(StartPage))
+        button2 = tk.Button(self,text="Reading Data...",command=lambda: Arduino.Arduino.readData())
+        button3 = tk.Button(self,text="Write to XML",command=lambda: Data.Data.prettyXml())
+        
+
+        button1.pack()
+        button2.pack()
+        button3.pack()
         
         
         
