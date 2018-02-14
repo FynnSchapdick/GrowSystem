cb8cbeb68118bab8a77bc3fe751a6910a4e5abc3
Fynn <Fynn@DESKTOP-7JDICME.fritz.box>
2018-02-14 10:32:34 +0100

    editet

--- src/Data.py
+++ src/Data.py
@@ -4,30 +4,29 @@ Created on 14.02.2018
 @author: Fynn
 '''
 import xml.etree.cElementTree as ET
-import Arduino
+from Arduino import Arduino
 
 class Data:
     
     #def __init__(self):
         
     done = False
-   
     names = ['m1','t1','h1','l1','time','date']
+    response = Arduino.response
     
-    def prettyXml(self,Arduino.response,Data.names):
-        
+    def prettyXml(self,response,names):
         data = []    
-        responseList = Arduino.response.split(sep=' ')
-        for index in range(len(Data.names)):
+        responseList = response.split(sep=' ')
+        for index in range(len(names)):
             for i in range(len(responseList)):
-                if (Data.names[index]==responseList[i]):
+                if (names[index]==responseList[i]):
                     data.append(responseList[i+1])
                     index+1
     
                     root = ET.Element("tent")
     
         for y in range(0,len(data)):
-            ET.SubElement(root,"data",name=Data.names[y]).text=data[y]
+            ET.SubElement(root,"data",name=names[y]).text=data[y]
         
         tree = ET.ElementTree(root)
         tree.write("data.xml")
