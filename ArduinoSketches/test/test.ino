1ac5a03fe80ee509cb4164375c076d6ff6de1a5d
Fynn <Fynn@DESKTOP-7JDICME.fritz.box>
2018-02-25 18:20:44 +0100

    editet

---
+++ ArduinoSketches/test/test.ino
@@ -0,0 +1,16 @@
+String incoming = "";
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    incoming = Serial.read();
+
+    Serial.print("Success");
+
+    Serial.println(incoming);
+  }
+
+}
