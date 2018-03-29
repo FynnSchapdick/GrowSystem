329f4a45781215f0af453f9380762292c4bddc4f
Fynn <Fynn@DESKTOP-7JDICME.fritz.box>
2018-03-29 22:45:14 +0200

    added Class Data

--- src/Data.py
+++ src/Data.py
@@ -1,40 +1,148 @@
 '''
-Created on 14.02.2018
+Created on 31.01.2018
 
 @author: Fynn
+@version: 0.0.1
 '''
-import xml.etree.cElementTree as ET
-from Arduino import Arduino
-import os
+
+"""
+    Class Data
+        recieveData() ***
+        addData(time,temperature,humidity,light) ***
+        getTemperature(datetime) ***
+        getHumidity(datetime)
+        getisLightON(datetime)
+        getTime()
+        getIsDry(datetime)
+        deleteData(datetime) ***
+        getList() ***
+
+"""
+
+import sqlite3
+import RPi.GPIO as GPIO
+import sys
+import Adafruit_DHT
+import datetime
 import time
 
+
 class Data:
+   
+    """
     
-    #def __init__(self):
+    Constructor which prepares access to Database
+    
+    """
+    def __init__(self):
         
-    done = False
-    names = ['m1','t1','h1','l1','time','date']
-    #response = Arduino.response
-    response = ['Start','m1','0.123','h1','0.134','date','3.34','t1','h1','l1','time','date']
-    
-    def prettyXml(self,response,names):
-        data = []    
-        responseList = response.split(sep=' ')
-        for index in range(len(names)):
-            for i in range(len(responseList)):
-                if (names[index]==responseList[i]):
-                    data.append(responseList[i+1])
-                    index+1
-    
-        root = ET.Element("tent")
-    
-        for y in range(0,len(data)):
-            ET.SubElement(root,"data",name=names[y]).text=data[y]
+        path = "/home/pi/data.db"
+        self.conn = sqlite3.connect(path)
+        self.c = self.conn.cursor()
         
-        tree = ET.ElementTree(root)
-        path = os.environ()
-        if os.path.exists(path) == True:
-            tree.write(time.strftime("%d-%m-%Y", time.localtime())+' '+ time.strftime("%H:%M:%S" , time.localtime()))
-            done = True
-             
+
+    """
+    
+    Destructor which closes access to database
+    
+    """    
+    def __del__(self):
+        self.conn.close()
+
+    
+    """
+    
+    Recieves data from Sensors
+    
+    """
+    def recieveData(self):
+        dht_PIN = 4
+        lightisOn_PIN = 17
+        isDry_PIN = 27
         
+        #GPIO.cleanup()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(dht_PIN,GPIO.IN)
+        GPIO.setup(lightisOn_PIN,GPIO.IN)
+        GPIO.setup(isDry_PIN,GPIO.IN)
+        
+        list = []
+        d = time.strftime("%d-%m-%Y %a %H:%M:%S", time.localtime())
+        #d = datetime.datetime.now()
+        h, t = Adafruit_DHT.read_retry(11, dht_PIN)
+        w = GPIO.input(isDry_PIN)
+        l = GPIO.input(lightisOn_PIN)
+        if(l!=1):
+            l=1
+        else:
+            l=0
+        #print('Datetime:',d,'Temp:',t,'Humidity:',h,'LightON:',l,'isDry:',w)
+            
+        list.append(d)
+        list.append(h)
+        list.append(t)
+        list.append(l)
+        list.append(w)    
+        Data.addData(self,d,t,h,l,w)
+        return list
+    
+        
+    """
+    
+    Add data to database
+    
+    """
+    def addData(self,datetime,temperature,humidity,light,water):
+        #Checking params datatype
+        assert(isinstance(datetime, str))
+        assert(isinstance(temperature, float))
+        assert(isinstance(humidity, float))
+        assert(isinstance(light,int))
+        assert(isinstance(water, int))
+        
+        params = (datetime,temperature,humidity,light,water)
+        sql = "INSERT INTO Datalist VALUES (NULL,?,?,?,?,?)"
+        self.c.execute(sql,params)
+        self.conn.commit()
+        
+        
+    """
+    
+    Get temperature by given datetime
+    
+    """        
+    def getTemperature(self,datetime):
+        assert(isinstance(datetime, str))
+        list = []
+        params = (datetime,)
+        sql = "SELECT TEMPERATURE, DATE FROM \
+        Datalist WHERE DATE = ?"
+        self.c.execute(sql,params)
+        for row in self.c.fetchall():
+            list.append(str(row[0]))
+        return list    
+    
+    
+    """
+    
+    Deletes data on database by given datetime
+    
+    """
+    def deleteData(self,datetime):
+        #Checking params datatype
+        assert(isinstance(datetime, str))
+        params = (datetime,)
+        sql = "DELETE FROM Datalist WHERE DATE = ? "
+        self.c.execute(sql,params)
+        self.conn.commit()
+       
+        
+    """
+    
+    Returns all data from database
+    
+    """
+    def getList(self):
+        sql = "SELECT * FROM Datalist"
+        self.c.execute(sql)
+        return self.c.fetchall()
